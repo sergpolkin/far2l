38bf38dd999d2af62d9cacad43d5f9df8ddcfa56
serg <sergpolkin@gmail.com>
2023-11-14 08:05:21 -0500

    add plugins build

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const plugins = @import("plugins/build.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -154,91 +155,13 @@ pub fn build(b: *std.Build) void {
 
     b.getInstallStep().dependOn(&b.addInstallArtifact(gui, .{ .dest_dir = .{ .override = .{ .custom = "install" } } }).step);
 
-    // plugins
-
-    const plug_simple = b.addSharedLibrary(.{
-        .name = "simple",
-        .root_source_file = .{ .path = "plugins/simple.zig" },
+    plugins.build(b, .{
         .target = target,
         .optimize = optimize,
-    });
-    b.getInstallStep().dependOn(&b.addInstallArtifact(plug_simple, .{
-        .dest_sub_path = "Plugins/simple/plug/simple.far-plug-wide",
-        .dest_dir = .{ .override = .{ .custom = "install" } },
-    }).step);
-
-    const plug_align = b.addSharedLibrary(.{
-        .name = "align",
-        .target = target,
-        .optimize = optimize,
-    });
-    plug_align.addCSourceFiles(.{
-        .dependency = upstream,
-        .files = &.{
-            "align/src/Align.cpp",
-        },
-        .flags = &.{},
-    });
-    plug_align.addIncludePath(upstream.path("align"));
-    plug_align.addIncludePath(upstream.path("far2l/far2sdk"));
-    plug_align.addIncludePath(upstream.path("WinPort"));
-    plug_align.addIncludePath(upstream.path("utils/include"));
-    plug_align.defineCMacro("UNICODE", null);
-    plug_align.defineCMacro("WINPORT_DIRECT", null);
-    plug_align.defineCMacro("FAR_DONT_USE_INTERNALS", null);
-    plug_align.linkLibrary(utils);
-    plug_align.linkLibCpp();
-    b.getInstallStep().dependOn(&b.addInstallArtifact(plug_align, .{
-        .dest_sub_path = "Plugins/align/plug/align.far-plug-wide",
-        .dest_dir = .{ .override = .{ .custom = "install" } },
-    }).step);
-    b.installDirectory(.{
-        .source_dir = upstream.path("align/configs/plug"),
-        .install_dir = .{ .custom = "install" },
-        .install_subdir = "Plugins/align/plug",
-    });
-
-    const plug_calc = b.addSharedLibrary(.{
-        .name = "calc",
-        .target = target,
-        .optimize = optimize,
-    });
-    plug_calc.addCSourceFiles(.{
-        .dependency = upstream,
-        .files = &plug_calc_src,
-        .flags = &.{},
-    });
-    plug_calc.addIncludePath(upstream.path("calc/src/shared"));
-    plug_calc.addIncludePath(upstream.path("far2l/far2sdk"));
-    plug_calc.addIncludePath(upstream.path("WinPort"));
-    plug_calc.addIncludePath(upstream.path("utils/include"));
-    plug_calc.defineCMacro("UNICODE", null);
-    plug_calc.defineCMacro("_UNICODE", null);
-    plug_calc.defineCMacro("TTMATH_NOASM", null);
-    plug_calc.linkLibrary(utils);
-    plug_calc.linkLibCpp();
-    b.getInstallStep().dependOn(&b.addInstallArtifact(plug_calc, .{
-        .dest_sub_path = "Plugins/calc/plug/calc.far-plug-wide",
-        .dest_dir = .{ .override = .{ .custom = "install" } },
-    }).step);
-    b.installDirectory(.{
-        .source_dir = upstream.path("calc/configs"),
-        .install_dir = .{ .custom = "install" },
-        .install_subdir = "Plugins/calc/plug",
+        .far2l_upstream = upstream,
+        .far2l_utils = utils,
     });
 }
-
-const plug_calc_src = [_][]const u8{
-    "calc/src/plugcalc/api.cpp",
-    "calc/src/plugcalc/api-far2.cpp",
-    "calc/src/plugcalc/calc.cpp",
-    "calc/src/plugcalc/config.cpp",
-    "calc/src/plugcalc/newparse.cpp",
-    "calc/src/plugcalc/sarg.cpp",
-    "calc/src/plugcalc/syntax.cpp",
-    "calc/src/shared/sgml/sgml.cpp",
-    "calc/src/shared/trex/trex.c",
-};
 
 const far2l_gui_src = [_][]const u8{
     "WinPort/src/Backend/WX/Paint.cpp",
